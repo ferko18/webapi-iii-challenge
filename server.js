2bdb177ea2474f25e424b4ae1a4f2b8a97ac743b
Firealem Erko <firealemg@yahoo.com>
2019-05-29 15:30:23 -0400

    added logger

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const server = express();
 
+
+server.use(logger)
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
@@ -8,7 +10,8 @@ server.get('/', (req, res) => {
 //custom middlewares
 
 function logger(req, res, next) {
-
+ console.log(`A ${req.method} request was made to ${req.url} at ${Date.now()}`)
+ next()
 };
 
 module.exports = server;
