4addd22dd2ba5b3fa2ca37130aca574a393866c3
Firealem Erko <firealemg@yahoo.com>
2019-05-29 15:47:12 -0400

    wired up server and routes

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const server = express();
 
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
+
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
+server.use(express.json());
 
 server.use(logger)
 server.get('/', (req, res) => {
